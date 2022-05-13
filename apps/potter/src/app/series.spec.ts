aaf113fa545256ee953113ff6e4f7c0a2bf51785
Azetry <azetry.bt10@nycu.edu.com>
2022-05-14 01:40:33 +0800

    second test: zero book/rewrite frist test

--- apps/potter/src/app/series.spec.ts
+++ apps/potter/src/app/series.spec.ts
@@ -3,6 +3,7 @@ import { Series } from './series';
 
 describe('Series', () => {
   let series: Series;
+  let num_books = 5;
 
   beforeEach(()=>{
     series = new Series();
@@ -12,14 +13,24 @@ describe('Series', () => {
     expect(series).toBeTruthy();
   });
 
+  test('zero book', () =>{
+    checkout([0,0,0,0,0]);
+    expect(series.price).toBe(0);
+  })
+
   test('one book', () =>{
-    checkout([1,0,0,0,0]);
-    expect(series.price).toBe(8);
+    for (let i = 0; i < num_books; i++) {
+      let cart = [0,0,0,0,0];
+      cart[i] = 1;
+      checkout(cart);
+      expect(series.price).toBe(8);
+    }
   })
 
   // Functions
   function checkout(books: number[]) {
-    for (let i = 0; i < books.length; i++) {
+    series.clear();
+    for (let i = 0; i < num_books; i++) {
       series.buy(i, books[i]);
     }
   }
