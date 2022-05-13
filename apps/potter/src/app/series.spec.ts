1e0fe026087083bef5ba9b0f1381907f792e41bf
Azetry <azetry.bt10@nycu.edu.com>
2022-05-13 16:19:39 +0800

    first test: one book

--- apps/potter/src/app/series.spec.ts
+++ apps/potter/src/app/series.spec.ts
@@ -1,7 +1,26 @@
+import { BuiltinType } from '@angular/compiler';
 import { Series } from './series';
 
 describe('Series', () => {
-  it('should create an instance', () => {
-    expect(new Series()).toBeTruthy();
+  let series: Series;
+
+  beforeEach(()=>{
+    series = new Series();
   });
+
+  it('should create an instance', () => {
+    expect(series).toBeTruthy();
+  });
+
+  test('one book', () =>{
+    checkout([1,0,0,0,0]);
+    expect(series.price).toBe(8);
+  })
+
+  // Functions
+  function checkout(books: number[]) {
+    for (let i = 0; i < books.length; i++) {
+      series.buy(i, books[i]);
+    }
+  }
 });
--- apps/potter/src/app/series.ts
+++ apps/potter/src/app/series.ts
@@ -1,2 +1,13 @@
 export class Series {
+  private books: number[] = [0,0,0,0,0];
+
+  buy(book: number, amount:number) {
+    this.books[book] += amount;
+  }
+
+  get price() {
+    let price = 0;
+
+    return price;
+  }
 }
